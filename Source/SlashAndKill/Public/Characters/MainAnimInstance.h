f338fbff24bb9a9dcda4fee55aba289805ce0922
Fillipe Romero <fillipe.romero@gmail.com>
2023-01-28 17:42:33 -0700

    add custom animInstance for main character

---
+++ Source/SlashAndKill/Public/Characters/MainAnimInstance.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAnimInstance.generated.h"
+
+class AMainCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class SLASHANDKILL_API UMainAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<AMainCharacter> MainCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	TObjectPtr<UCharacterMovementComponent> MainCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float GroundSpeed{ 0.f };
+
+protected:
+
+private:
+
+public:
+	
+};
